19bc6d8d2772f56f7586ff633dab6bf28b7cd29a
TomYenialem <tom21g2008@gmail.com>
2024-11-30 09:58:13 +0300

    login and setting

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import Chat from "./Components/Chat/Chat";
+import Setting from "./Components/Setting/Setting";
 import User from "./Components/UserInfo/User";
+import Login from './Components/Login/Login'
 
 
 
@@ -8,15 +11,24 @@ import User from "./Components/UserInfo/User";
 
 function App() {
  
-
+ const [show, setShow] = useState(false)
+  const [showChat, setShowCaht] = useState(false);
   return (
     <>
-    
+      <div className={` ${show ? "bg" : "container"}`}>
+        {showChat ? (
+          <>
+            <Login/>
+            {/* <Notify /> */}
+          </>
+        ) : (
           <div className="body-lists">
-            <User/>
-            <Chat/>
+            <User />
+            <Chat />
+            <Setting show={show} setShow={setShow} />
           </div>
-       
+        )}
+      </div>
     </>
   );
 }
